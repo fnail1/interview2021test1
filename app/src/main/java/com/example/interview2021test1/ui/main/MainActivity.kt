041ddd48042dae6c26ff7594ebff125982d46ff7
Fedor Gvozdev <f.gvozdev@corp.mail.ru>
2022-05-26 10:54:29 +0300

    AppCenter

--- app/src/main/java/com/example/interview2021test1/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/interview2021test1/ui/main/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.interview2021test1.ui.main
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.interview2021test1.databinding.ActivityMainBinding
 import com.example.interview2021test1.model
 import com.example.interview2021test1.model.CurrencyModel
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+import kotlin.concurrent.thread
+
 
 class MainActivity : AppCompatActivity(),
     CurrencyModel.DataChangeListener,
@@ -17,6 +23,10 @@ class MainActivity : AppCompatActivity(),
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        AppCenter.start(application, "fdc79b44-659a-444a-bd98-96765ba8be19",
+            Analytics::class.java, Crashes::class.java)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
@@ -34,6 +44,15 @@ class MainActivity : AppCompatActivity(),
     override fun onResume() {
         super.onResume()
         model().currency.addDataChangeListener(this)
+
+        thread { th1() }
+        binding.list.postDelayed({
+            val thread = thread { th2() }
+            binding.list.postDelayed({
+                thread.interrupt()
+                Log.v("SYNC_TEST", "th2.interrupt")
+            }, 1000)
+        }, 1000)
     }
 
     override fun onPause() {
@@ -51,6 +70,36 @@ class MainActivity : AppCompatActivity(),
     @SuppressLint("NotifyDataSetChanged")
     override fun onRefresh() {
         model().currency.reset()
+    }
+
+    val monitor = Any()
+    fun th1() {
+        Log.v("SYNC_TEST", "th1.in")
+        synchronized(monitor) {
+            Log.v("SYNC_TEST", "th1.start")
+            for (i in 0 until 10000000) {
+                buildString {
+                    append("SYNC_TEST")
+                    append(" - th1 - ")
+                    append(i)
+                }
+
+                if (i % 1000 == 0)
+                    Log.v("SYNC_TEST", "th1.i=$i")
+            }
+
+            Log.v("SYNC_TEST", "th1.complete")
+        }
+        Log.v("SYNC_TEST", "th1.out")
+    }
+
+    fun th2() {
+        Log.v("SYNC_TEST", "th2.in")
+        synchronized(monitor) {
+            Log.v("SYNC_TEST", "th2.start")
+            Log.v("SYNC_TEST", "th2.complete")
+        }
+        Log.v("SYNC_TEST", "th2.out")
     }
 
 }
